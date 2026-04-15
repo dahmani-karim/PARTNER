9a0436369747a837ece74f0eb4860c22546372ec
dahmani-karim <karim.dahmani88@gmail.com>
2026-04-15 15:55:43 +0200

    feat: add push notification handler to service worker

--- vite.config.js
+++ vite.config.js
@@ -42,6 +42,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        importScripts: ['push-sw.js'],
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
